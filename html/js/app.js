9f34c433a15ee711f490084c2a7a08d89c633665
ItzDabbzz <itzdabbzz@gmail.com>
2023-05-05 15:23:20 -0700

    Added Vehicle Key

--- html/js/app.js
+++ html/js/app.js
@@ -520,6 +520,9 @@ function FormatItemInfo(itemData, dom) {
             $(".item-info-description").html(
                 "<p>" + itemData.info.uses + " uses left.</p>"
             );
+        } else if (itemData.name == "vehiclekey") {
+            $(".item-info-title").html('<p>' + itemData.info.model + '</p>');
+            $(".item-info-description").html('<p>Plate : ' + itemData.info.plate + '</p>');
         } else if (itemData.type == "weapon") {
             $(".item-info-title").html("<p>" + itemData.label + "</p>");
             if (itemData.info.ammo == undefined) {
